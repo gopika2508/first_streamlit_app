30942b6fe50311bdbf5ad432ddfe25c1e1b7349c
gopika2508 <112179559+gopika2508@users.noreply.github.com>
2022-08-26 19:46:10 +0530

    Update streamlit_app.py
    added fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, Spinach & Rocket Smoothie')
-streamli.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍲 Breakfast Menu')
+streamlit.text('🌮 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
